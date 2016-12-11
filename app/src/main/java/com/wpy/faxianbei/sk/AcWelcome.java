d9f35238fe3fd13b99a0e98e383063e822dcc9c2
peiyuwang <3285840393@qq.com>
2016-12-11 16:49:59 +0800

    初始化app

---
+++ app/src/main/java/com/wpy/faxianbei/sk/AcWelcome.java
@@ -0,0 +1,13 @@
+package com.wpy.faxianbei.sk;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class AcWelcome extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.ac_welcome);
+    }
+}
