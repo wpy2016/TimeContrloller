5ee542d9410fd6ce06a83e8e0190f5609c02a005
peiyuwang <3285840393@qq.com>
2016-12-18 00:29:06 +0800

    完成了腾讯云架包的导入，并实现了一个测试的例子

--- app/src/main/java/com/wpy/faxianbei/sk/activity/others/AcWelcome.java
+++ app/src/main/java/com/wpy/faxianbei/sk/activity/others/AcWelcome.java
@@ -2,7 +2,12 @@ package com.wpy.faxianbei.sk.activity.others;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.util.Log;
 
+import com.avos.avoscloud.AVException;
+import com.avos.avoscloud.AVOSCloud;
+import com.avos.avoscloud.AVObject;
+import com.avos.avoscloud.SaveCallback;
 import com.wpy.faxianbei.sk.R;
 
 /**
@@ -14,5 +19,18 @@ public class AcWelcome extends CheckPermissionsActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.ac_welcome);
+        // 初始化参数依次为 this, AppId, AppKey
+        AVOSCloud.initialize(this,"FFwHvC1gi4JDqPnfqkOmshDH-9Nh9j0Va","aLETvSFc2y1G2jmBWeBpSX96");
+        // 测试 SDK 是否正常工作的代码
+        AVObject testObject = new AVObject("TestObject");
+        testObject.put("words","Hello World!");
+        testObject.saveInBackground(new SaveCallback() {
+            @Override
+            public void done(AVException e) {
+                if(e == null){
+                    Log.d("saved","success!");
+                }
+            }
+        });
     }
 }
--- app/src/main/java/com/wpy/faxianbei/sk/activity/others/CheckPermissionsActivity.java
+++ app/src/main/java/com/wpy/faxianbei/sk/activity/others/CheckPermissionsActivity.java
@@ -33,6 +33,10 @@ public class CheckPermissionsActivity extends Activity
 	 */
 	protected String[] needPermissions = {
 			Manifest.permission.INTERNET,
+			Manifest.permission.WRITE_EXTERNAL_STORAGE,
+			Manifest.permission.ACCESS_NETWORK_STATE,
+			Manifest.permission.READ_PHONE_STATE,
+			Manifest.permission.ACCESS_WIFI_STATE
 			};
 	/**
 	 * 子Activity传入的perimission
