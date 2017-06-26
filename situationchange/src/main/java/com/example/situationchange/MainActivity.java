36970da0b562045b3ce19a5b23c1cf0c457b968e
wpy2016 <3285840393@qq.com>
2017-06-26 10:54:34 +0800

    situation模块情景模式切换demo测试成功

--- situationchange/src/main/java/com/example/situationchange/MainActivity.java
+++ situationchange/src/main/java/com/example/situationchange/MainActivity.java
@@ -1,12 +1,12 @@
 package com.example.situationchange;
 
-import android.app.Activity;
+import android.media.AudioManager;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
 
-public class MainActivity extends Activity implements View.OnClickListener {
+public class MainActivity extends CheckPermissionsActivity implements View.OnClickListener {
 
     Button btnSlient;
 
@@ -15,6 +15,7 @@ public class MainActivity extends Activity implements View.OnClickListener {
     Button btnLing;
 
     TextView tvSituation;
+    AudioManager manager;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -22,13 +23,15 @@ public class MainActivity extends Activity implements View.OnClickListener {
         setContentView(R.layout.activity_main);
         initView();
         initEvent();
+        manager = (AudioManager) MainActivity.this.getSystemService(AUDIO_SERVICE);
+        tvSituation.setText(situation(manager.getRingerMode()));
     }
 
     private void initView() {
-        btnSlient = findViewById(R.id.id_ac_main_btn_changto_slient);
-        btnShake = findViewById(R.id.id_ac_main_btn_changto_shake);
-        btnLing = findViewById(R.id.id_ac_main_btn_changto_ling);
-        tvSituation = findViewById(R.id.id_ac_main_tv_situation);
+        btnSlient = (Button) findViewById(R.id.id_ac_main_btn_changto_slient);
+        btnShake = (Button) findViewById(R.id.id_ac_main_btn_changto_shake);
+        btnLing = (Button) findViewById(R.id.id_ac_main_btn_changto_ling);
+        tvSituation = (TextView) findViewById(R.id.id_ac_main_tv_situation);
     }
 
     private void initEvent() {
@@ -42,18 +45,32 @@ public class MainActivity extends Activity implements View.OnClickListener {
         switch (view.getId()) {
 
             case R.id.id_ac_main_btn_changto_slient:
-
+                manager.setRingerMode(AudioManager.RINGER_MODE_SILENT);
                 break;
             case R.id.id_ac_main_btn_changto_shake:
-
+                manager.setRingerMode(AudioManager.RINGER_MODE_VIBRATE);
                 break;
             case R.id.id_ac_main_btn_changto_ling:
-
+                manager.setRingerMode(AudioManager.RINGER_MODE_NORMAL);
                 break;
         }
+        tvSituation.setText(situation(manager.getRingerMode()));
     }
 
-    private void changeSituation(){
-
+    private String situation(int id) {
+        String model=null;
+        switch (id) {
+            case AudioManager.RINGER_MODE_SILENT:
+                model="静音";
+                break;
+            case AudioManager.RINGER_MODE_VIBRATE:
+                model="震动";
+                break;
+            case AudioManager.RINGER_MODE_NORMAL:
+                model="响铃";
+                break;
+        }
+        return model;
     }
+
 }
